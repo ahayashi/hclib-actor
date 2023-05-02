6e7ce18e69f2c23eb743919d08165114123619a5
Jiawei Yang <joseph960616@gmail.com>
2023-05-02 13:37:53 -0400

    update installation scripts

--- cluster-scripts/perlmutter_setup.sh
+++ cluster-scripts/perlmutter_setup.sh
@@ -16,26 +16,31 @@ export CXX=CC
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/cray/pe/perftools/23.02.0/lib64
 
 # Setup Bale
-git clone https://github.com/jdevinney/bale.git bale
-cd bale
-wget https://raw.githubusercontent.com/ahayashi/hclib-actor/master/cluster-scripts/perlmutter.patch
-patch -p1 < perlmutter.patch
-cd ..
-cd bale/src/bale_classic
-./bootstrap.sh
-python3 ./make_bale -s
-cd ../../../
+if [ ! -d bale ]; then
+    git clone https://github.com/jdevinney/bale.git bale
+    cd bale
+    wget https://raw.githubusercontent.com/ahayashi/hclib-actor/master/cluster-scripts/perlmutter.patch
+    patch -p1 < perlmutter.patch
+    cd ..
+    cd bale/src/bale_classic
+    export BALE_INSTALL=$PWD/build_${PLATFORM}
+    ./bootstrap.sh
+    python3 ./make_bale -s
+    cd ../../../
+fi
 
 # Setup HCLib
-git clone https://github.com/srirajpaul/hclib
-cd hclib
-git fetch && git checkout bale3_actor
-./install.sh --enable-production
-source hclib-install/bin/hclib_setup_env.sh
-cd modules/bale_actor && make
-cd benchmarks
-unzip ../inc/boost.zip -d ../inc/
-cd ../../../../
+if [ ! -d hclib ]; then
+    git clone https://github.com/srirajpaul/hclib
+    cd hclib
+    git fetch && git checkout bale3_actor
+    ./install.sh --enable-production
+    source hclib-install/bin/hclib_setup_env.sh
+    cd modules/bale_actor && make
+    cd benchmarks
+    unzip ../inc/boost.zip -d ../inc/
+    cd ../../../../
+fi
 
 export BALE_INSTALL=$PWD/bale/src/bale_classic/build_${PLATFORM}
 export HCLIB_ROOT=$PWD/hclib/hclib-install
